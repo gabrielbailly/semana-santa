46e9ab248717bc80554e51e8f7a56d4957d388d5
gabrielbailly <158313900+gabrielbailly@users.noreply.github.com>
2026-03-14 23:58:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ const LEVEL_LABELS = {
 };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBW4WQ6hXqEWEd0AHCP5aXeU0m1bplQIk",
-  authDomain: "gen-lang-client-0498976958.firebaseapp.com",
-  projectId: "gen-lang-client-0498976958",
-  storageBucket: "gen-lang-client-0498976958.firebasestorage.app",
-  messagingSenderId: "926052083535",
-  appId: "1:926052083535:web:a6438c54b67c1b3491c73a",
+  apiKey: "AIzaSyB3HryH0Mg996QV0HUdaxYox4ah5xDnYJM",
+  authDomain: "semana-santa-f9967.firebaseapp.com",
+  projectId: "semana-santa-f9967",
+  storageBucket: "semana-santa-f9967.firebasestorage.app",
+  messagingSenderId: "85757700422",
+  appId: "1:85757700422:web:7b762e3a9ac9be34c3dd47"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
